c7f3b93c385a7904bdefec3a4503c9c03ceef03e
gat41 <15726053+gat41@users.noreply.github.com>
2020-10-19 15:27:07 +0800

    Added comments and divided code into sections

--- task.py
+++ task.py
@@ -1,12 +1,15 @@
 # The python task
-
+#%% import libraries
 import random
+
+#%% define functions
 def div_and_write(L):
     new =[]
     # create a list of list
     new.append(L)
     count =1
     f = open('result.txt', 'a+', encoding='Utf-8')
+
     # while loop for reading and writing
     while len(new)!=0:
     # take the first list and delete it
@@ -16,9 +19,11 @@ def div_and_write(L):
      m1 = min(L3)
      m2 = max(L3)
      d = (m1 + m2) / 2
-     f.write('{}  min:{}  min:{}  length:{} \n'.format(count, m1, m2, len(L3)))
+     f.write('{}  min:{}  min:{}  length:{} \n'.format(count, m1, m2, len(L3)))  
+     # perhaps you can remove the min, max, length, then you do not need to put your code in a data frame
+     # does the "write" function allow for separators such as \t, if yes, then maybe you have something like count \t m1 \m2
      count +=1
-     # if length>5, the divide into two part(二分)
+     # if length>5, the divide into two parts
      if len(L3) > 5:
         L2 = []
         L1 = []
@@ -33,6 +38,8 @@ def div_and_write(L):
         new.append(L2)
 
     return
+
+#%% main programme
 # create a random list
 a=[ random.uniform(0,100) for i in range(100)]
 div_and_write(a)
